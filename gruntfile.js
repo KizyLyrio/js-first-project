37f384f501897f24ac2c28697bce9826c7d442b5
kizylyrio <kizylirio@gmail.com>
2017-04-11 12:07:38 -0300

    Instalação do bower

--- gruntfile.js
+++ gruntfile.js
@@ -1,25 +1,24 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
 
-//Grunt config
-grunt.initConfig({
-    //css
-    cssmin:{
-        target:{
-            files:{
-                'public/css/style.min.css': [
-                    'assets/css/main.css',
-                    'assets/css/typography.css'
-                ]
+    //Grunt config
+    grunt.initConfig({
+        cssmin: {
+            target: {
+                files: {
+                    'public/css/style.min.css': [
+                        'assets/css/main.css',
+                        'assets/css/typography.css'
+                    ]
+                }
             }
         }
-    }
 
-});
+    });
 
-//Load plugins
-grunt.loadNpmTasks('grunt-contrib-cssmin');
+    //Load plugins
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-//Tasks
-grunt.registerTask('css',['cssmin']);
+    //Tasks
+    grunt.registerTask('css', ['cssmin']);
 
 };
